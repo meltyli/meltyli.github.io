3e8d6f257301cf88f0aca18cacd18dcdf1b97452
meltyli <meltyli@outlook.com>
2023-08-15 07:32:10 +1000

    committing bc it's getting too big

---
+++ aos.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    AOS.init({
+        duration: 800, // Animation duration
+        easing: "ease-in-out", // Easing for animation
+    });
+});
---
+++ script.js
@@ -0,0 +1,20 @@
+// Add animations using JavaScript
+document.addEventListener("DOMContentLoaded", function () {
+    const ctaButton = document.querySelector(".cta-button");
+    const features = document.querySelectorAll(".feature");
+
+    // Add animation to CTA button
+    ctaButton.addEventListener("mouseover", function () {
+        this.classList.add("cta-button-hover");
+    });
+
+    // Add animation to features on scroll
+    window.addEventListener("scroll", function () {
+        features.forEach((feature) => {
+            const position = feature.getBoundingClientRect().top;
+            if (position < window.innerHeight - 50) {
+                feature.classList.add("feature-animate");
+            }
+        });
+    });
+});
