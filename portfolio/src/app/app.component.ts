cf7c3f78ccc5f3af0214bd7aa79cec5989ff5e8a
meltyli <meltyli@outlook.com>
2023-08-15 11:26:07 +1000

    moved to animation.css

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', 'animations.css']
 })
 export class AppComponent {
   title = 'portfolio';
