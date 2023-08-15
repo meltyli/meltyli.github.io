30f352b7d4d2775071de56a98a5ce7de8e05a482
meltyli <meltyli@outlook.com>
2023-08-15 16:34:20 +1000

    fixed padding for nav bar add timeline animate timeline

--- portfolio/src/app/about/about.component.ts
+++ portfolio/src/app/about/about.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // JavaScript for the interactive timeline
+    const events = document.querySelectorAll('.event');
+    events.forEach(event => {
+      event.addEventListener('mouseenter', () => {
+        event.classList.add('active');
+      });
+      event.addEventListener('mouseleave', () => {
+        event.classList.remove('active');
+      });
+    });
+  }
 
 }
