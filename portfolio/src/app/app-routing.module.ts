686619386c6a62e4b4fb037f0b882e28bcc58989
meltyli <meltyli@outlook.com>
2023-08-15 11:21:37 +1000

    cont. merge changes to node

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MainComponent } from './main/main.component';
+
+const routes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: '', redirectTo: 'main', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
